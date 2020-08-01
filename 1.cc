5fb97af5d8c8640872bc20f9a0850df5e4490c85
KingTreeS <956255917@qq.com>
2020-08-01 18:47:30 +0800

    1

--- 1.cc
+++ 1.cc
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 # include <bits/stdc++.h>
 using namespace std;
 
 int main() {
 
-=======
-int main()
-{
-    // test
-    return 0;
->>>>>>> f61232d26827cdfcd3d5ed7a44c8c2c5502c22c1
 }
