dad6a793d07f38416be72591fb6f0b2efa19714c
shonejiang <shonejiang@tencent.com>
2020-08-01 19:46:56 +0800

    1

--- 3.cc
+++ 3.cc
@@ -1,7 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
+strcut Classes {
+    int begin, end;
+    Classes()
+}
 
 int main() {
-    int n
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+
+    }
  
 }
