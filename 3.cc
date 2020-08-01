9beb1f69deb83be06550195b70842b02ba80ed8b
shonejiang <shonejiang@tencent.com>
2020-08-01 19:49:54 +0800

    1

--- 3.cc
+++ 3.cc
@@ -6,15 +6,21 @@ strcut Classes {
         begin = a; end = b;
     }
     bool operator<(Classes& rhs) {
+        if (this->begin == rhs.begin) {
+            return this->end == rhs.end
+        }
         return this->begin < rhs.begin;
     }
-}
+};
 
 int main() {
     int n;
     cin >> n;
+    vector<Classes> p;
     for (int i = 0; i < n; i++) {
-
+        int a,b;
+        cin >> a >> b;
+        p.push_back(Classes())
     }
  
 }
