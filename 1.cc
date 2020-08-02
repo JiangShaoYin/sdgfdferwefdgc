29a63062ef5dc6e343a98437f3c4ce117e581737
shonejiang <shonejiang@tencent.com>
2020-08-02 19:57:03 +0800

    1

--- 1.cc
+++ 1.cc
@@ -1,12 +1,28 @@
-# include <bits/stdc++.h>
-using namespace std;
-
-const int N = 
 
 
 int main() {
     int n;
     cin >> n;
-    map<int, vector<int>>
+    map<int, set<int>>  mp;
+    int cnt = 0;
+    map<pair<int, int>, int> ans;
+    for (int i = 0; i < n; i++) {
+        int a,b,c,d,e,f;
+        cin >> a >> b >> c >> d >> e >> f;
+        if (a > b) swap(a, b);
+        mp[10 * a + b].insert(1000 * c + 100 * d + 10 * e + f);
+        mp[10 * a + b].insert(1000 * c + 100 * d + 10 * f + e);
+
+        mp[10 * a + b].insert(1000 * d + 100 * c + 10 * f + e);
+        mp[10 * a + b].insert(1000 * d + 100 * c + 10 * e + f);
+        
+        if (mp.count(10 * a + b) || mp.count(10 * b + a)) {
+            int t = 1000 * c + 100 * d + 10 * e + f;
+            if (mp[10 * a + b].count(t) || mp[10 * b + a].count(t)) {
+                
+            }
+            if ()
+        }
+    }
 
 }
