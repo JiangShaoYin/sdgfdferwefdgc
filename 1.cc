f86bbe37a662a9045273211bdbeef2bd65971b3d
shonejiang <shonejiang@tencent.com>
2020-08-02 20:02:01 +0800

    1

--- 1.cc
+++ 1.cc
@@ -5,29 +5,19 @@ int main() {
     cin >> n;
     map<int, set<int>>  mp;
     int cnt = 0;
-    map<pair<int, int>, int> ans;
+    map<int, int> ans;
     for (int i = 0; i < n; i++) {
         int a,b,c,d,e,f;
         cin >> a >> b >> c >> d >> e >> f;
         if (a > b) swap(a, b);
 
-        mp[10 * a + b].insert(1000 * c + 100 * d + 10 * e + f);
-        mp[10 * a + b].insert(1000 * c + 100 * d + 10 * f + e);
 
-        mp[10 * a + b].insert(1000 * d + 100 * c + 10 * f + e);
-        mp[10 * a + b].insert(1000 * d + 100 * c + 10 * e + f);
-
-        mp[10 * a + b].insert(1000 * e + 100 * f + 10 * c + d);
-        mp[10 * a + b].insert(1000 * e + 100 * f + 10 * d + c);
-
-        mp[10 * a + b].insert(1000 * f + 100 * e + 10 * c + d);
-        mp[10 * a + b].insert(1000 * f + 100 * e + 10 * d + c);
 
         
         int t = 1000 * c + 100 * d + 10 * e + f;
-        if (mp.count(10 * a + b) && mp[10 * a + b].count(1000 * c + 100 * d + 10 * e + f)) {
-                
-            if ()
+        if (mp.count(10 * a + b) && mp[10 * a + b].count(t)) { // 重复筛子
+            ans[10 * a + b]++;
+            continue;
         }
     }
 
